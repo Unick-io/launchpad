68239f9eaed7673bcefb8fe1310e49a093314f7c
Michal Dzianach <michaldzianach@iMac.local>
2023-04-12 18:15:21 +0100

    starter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { unlinkSync } from "fs";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
+          Welcome to unlinkSync.io NFT <a href="http://unick.io">launchpad</a>!
         </h1>
 
         <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
+        App is in development. Check back soon.
         </p>
 
         <div className={styles.connect}>
@@ -21,30 +20,13 @@ const Home: NextPage = () => {
         </div>
 
         <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
+          <h2>Checkout our marketplace</h2>
           <a
-            href="https://portal.thirdweb.com/templates"
+            href="https://marketplace.unick.io"
             className={styles.card}
           >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
+
           </a>
         </div>
       </main>
