33260e6520b75f1e3a0bdb93993c3d9fc758a4e5
Michal Dzianach <michaldzianach@iMac.local>
2023-04-13 15:37:03 +0100

    2nd commit

---
+++ components/navbar/index.tsx
@@ -0,0 +1,105 @@
+import { useRef } from 'react'
+import { Box, Flex } from '../primitives'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Image from 'next/image'
+import { ConnectWallet, useAddress, } from "@thirdweb-dev/react";
+import NavItem from './NavItem'
+import ThemeSwitcher from './ThemeSwitcher'
+import { useTheme } from 'next-themes'
+import { useMediaQuery } from 'react-responsive'
+import { useIsMounted } from 'usehooks-ts'
+
+
+export const NAVBAR_HEIGHT = 81
+export const NAVBAR_HEIGHT_MOBILE = 77
+
+
+
+const Navbar = () => {
+  const { theme } = useTheme()
+  const address = useAddress();
+  const isMounted = useIsMounted()
+
+  let searchRef = useRef<HTMLInputElement>(null)
+
+  const router = useRouter()
+
+  if (!isMounted) {
+    return null
+  }
+
+  return (
+    <Flex
+      css={{
+        height: NAVBAR_HEIGHT,
+        px: '$5',
+        width: '100%',
+        maxWidth: 1920,
+        mx: 'auto',
+        borderBottom: '1px solid $slate12',
+        zIndex: 999,
+        background: '$slate12',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+      }}
+      align="center"
+      justify="between"
+    >
+    <Box css={{ flex: 1 }}>
+    <Flex align="center">
+      <Link href="/">
+        <Box css={{ width: 112, cursor: 'pointer' }}>
+          {theme == 'dark' ? (
+            <Image
+              src="/unickLaunchpadDark.png"
+              width={112}
+              height={30}
+              alt="Unick.io"
+            />
+          ) : (
+            <Image
+              src="/unickLaunchpadLight.png"
+              width={112}
+              height={30}
+              alt="Unick.io"
+            />
+          )}
+        </Box>
+      </Link>
+      <Box css={{ flex: 1, px: '$5', maxWidth: '60%' }}>
+      </Box>
+      <Flex align="center" css={{ gap: '$5', mr: '$5' }}>
+        <Link href="/mint">
+          <NavItem active={router.pathname == '/collection-rankings'}>
+            Mint
+          </NavItem>
+        </Link>
+        <Link href="/stake">
+          <NavItem active={router.pathname == '/stake'}>Stake</NavItem>
+        </Link>
+        <Link href="https://marketplace.unick.io">
+        <NavItem active={false}>Marketplace</NavItem>
+      </Link>
+        <Link href="https://docs.unick.io">
+          <NavItem active={false}>Docs</NavItem>
+        </Link>
+      </Flex>
+    </Flex>
+  </Box>
+
+  <Flex css={{ gap: '$3' }} justify="end" align="center">
+    <ThemeSwitcher />
+
+    <Box css={{ maxWidth: '185px' }}>
+      <ConnectWallet />
+    </Box>
+  </Flex>
+  </Flex>
+
+  )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,35 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-import { unlinkSync } from "fs";
+import { Footer } from '../components/home/Footer';
+import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://unick.io">Unick.io</a>! NFT launchpad.
-        </h1>
+    <Layout>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <a href="http://unick.io">Unick.io</a> NFT launchpad.
+          </h1>
 
-        <p className={styles.description}>
-        App is in development. Check back soon.
-        </p>
+          <p className={styles.description}>
+          App is in development. Check back soon.
+          </p>
 
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
+          <div className={styles.grid}>
 
-        <div className={styles.grid}>
-
-          <h2>Checkout our marketplace</h2>
-          <a
-            href="https://marketplace.unick.io"
-            className={styles.card}
-          >
-            Marketplace.unick.io
-          </a>
-        </div>
-      </main>
-    </div>
+            <h2 className={styles.h2}>Check out our marketplace</h2>
+            <a
+              href="https://marketplace.unick.io"
+              className={styles.card}
+            >
+              Marketplace.unick.io
+            </a>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </Layout>
   );
 };
 
